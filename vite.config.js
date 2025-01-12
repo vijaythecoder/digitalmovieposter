420e97a6d0d9161b7cd6fc6c0e1e25193fe39747
Vijay Tupakula <hello@vijaykumar.me>
2025-01-11 22:17:20 -0600

    fix: add base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/digitalmovieposter/',
   plugins: [vue()],
   server: {
     hmr: {
